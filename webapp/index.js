8ae0c70dd873547ce6963828beff85653db30211
schmelto <tom.schmelzer@web.de>
2020-10-07 19:51:58 +0200

    Step 6: Resource Models

--- webapp/index.js
+++ webapp/index.js
@@ -1,8 +1,8 @@
 sap.ui.require([
 	"sap/ui/model/json/JSONModel",
 	"sap/ui/core/mvc/XMLView",
-	"sap/ui/model/BindingMode"
-], function (JSONModel, XMLView, BindingMode) {
+	"sap/ui/model/resource/ResourceModel"
+], function (JSONModel, XMLView, ResourceModel) {
 	"use strict";
 
 	// Attach an anonymous function to the SAPUI5 'init' event
@@ -16,7 +16,21 @@ sap.ui.require([
 
 		});
 
-		oModel.setDefaultBindingMode(BindingMode.OneWay);
+		// Create a resource bundle for language specific texts
+		// the configured supportedLocales represent the i18n files present:
+		// * "" - i18n/i18n.properties
+		// the configured fallbackLocale should represent one of these files
+		// * "" - according to the fallback chain the root bundle is the last fallback.
+		//   Configuring it explicitly avoids side effects when additional resource files are added.
+		// @see https://sapui5.hana.ondemand.com/#/topic/ec753bc539d748f689e3ac814e129563
+		var oResourceModel = new ResourceModel({
+			bundleName: "webapp.i18n.i18n",
+			supportedLocales: ["", "de"],
+			fallbackLocale: ""
+		});
+
+		// Assign the model object to the SAPUI5 core using the name "i18n"
+		sap.ui.getCore().setModel(oResourceModel, "i18n");
 
 
 		// Assign the model object to the SAPUI5 core
